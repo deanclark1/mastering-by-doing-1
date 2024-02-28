6e18009f52dcc85fd6f6cf732e71a5e73bb257b5
doreen11 <ntumydoreen@gmail.com>
2024-02-28 06:47:55 +0000

    added functionality

--- script.js
+++ script.js
@@ -1,20 +1,95 @@
-console.log("it is linked");
-
+let buffer = "";
+let runningTotal = 0;
+let previousOperator;
 let screen = document.querySelector(".screen");
+let buttons = document.querySelectorAll(".button");
 
-let buttons = document.querySelectorAll(".buttons");
-
-// buttons.addEventListener("click", (e) => {
-//   console.log("You have been clicked");
-// });
-
-for (let i = 0; i < buttons.length; i++) {
-  buttons[i].addEventListener("click", (e) => {
-    // console.log("I have been clicked");
-
-    let buttonText = e.target.textContent;
-
-    screen.textContent = buttonText;
-    console.log("button clicked is.. ", buttonText);
-  });
+function buttonClick(value) {
+  if (isNaN(parseInt(value))) {
+    handleSymbol(value);
+  } else {
+    handleNUmber(value);
+  }
+  rerender();
 }
+
+function handleNUmber(number) {
+  if (buffer === "0") {
+    buffer = number;
+  } else {
+    buffer = buffer + number;
+  }
+}
+
+function handleMath(value) {
+  if (buffer === 0) {
+    //do nothing
+    return;
+  }
+  const intBuffer = parseInt(buffer);
+  if (runningTotal === 0) {
+    runningTotal = intBuffer;
+  } else {
+    flushOperation(intBuffer);
+  }
+  previousOperator = value;
+  buffer = "0";
+  console.log(runningTotal);
+}
+function flushOperation(intBuffer) {
+  if (previousOperator === "+") {
+    runningTotal += intBuffer;
+  } else if (previousOperator === "-") {
+    runningTotal -= intBuffer;
+  } else if (previousOperator === "×") {
+    runningTotal *= intBuffer;
+  } else if (previousOperator === "÷") {
+    runningTotal /= intBuffer;
+  }
+}
+
+function handleSymbol(symbol) {
+  switch (symbol) {
+    case "C":
+      buffer = "0";
+      break;
+    case "=":
+      if (previousOperator === null) {
+        //need two numbers to do math
+        return;
+      }
+      flushOperation(parseInt(buffer));
+      previousOperator = null;
+      buffer = "" + runningTotal;
+      runningTotal = 0;
+      break;
+    case "←":
+      if (buffer.length === 1) {
+        buffer = "0";
+      } else {
+        buffer = buffer.substring(0, buffer.length - 1);
+      }
+      break;
+    case "+":
+    case "×":
+    case "÷":
+    case "-":
+      handleMath(symbol);
+      break;
+    default:
+      break;
+  }
+}
+
+function init() {
+  for (let i = 0; i < buttons.length; i++) {
+    buttons[i].addEventListener("click", (e) => {
+      buttonClick(e.target.textContent);
+    });
+  }
+}
+
+function rerender() {
+  screen.textContent = buffer;
+}
+init();
